2f22b417bb360bc81c644fb60b1caa8713dea6e2
Priyajit <priyajithzraxx@gmail.com>
2018-03-15 09:31:36 +0530

    Ok

--- src/test/java/Parallel_Runner/Parallel_Runner.java
+++ src/test/java/Parallel_Runner/Parallel_Runner.java
@@ -42,8 +42,6 @@ import java.io.File;
 import java.io.IOException;
 import org.junit.AfterClass;
 import org.junit.runner.RunWith;
-
-
 public class Parallel_Runner {
 	  @Test
 	public void runAllTests() throws IOException, InterruptedException {
